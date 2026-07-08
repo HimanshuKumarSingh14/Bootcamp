a44d75c409115b37265a529f7dcc0ff822d55edf
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-08 23:31:52 +0530

    s_largest

--- ARRAY/s_largest.cpp
+++ ARRAY/s_largest.cpp
@@ -10,6 +10,24 @@ int main()
         cin>>arr[i];
 
     }
-    
+    int largest=arr[0];
+    int s_largest=-1;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>largest)
+        {
+            s_largest=largest;
+            largest=arr[i];
+            
+        }
+        else if(arr[i]<largest && arr[i]>s_largest)
+        {
+            s_largest=arr[i];
+        }
+        else{
+            continue;
+        }
+    }
+    cout<<"second largest"<<s_largest;
     return 0;
 }
