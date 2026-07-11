215c9e7c128ea2c1352958f9e4f072fd5ee3ac42
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-11 18:14:20 +0530

    remove_duplicates

--- string/remove_duplicates.cpp
+++ string/remove_duplicates.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
-#include<set>
+#include<unordered_set>
 using namespace std;
 int main()
 {
     string s;
     cin>>s;
-    
+    unordered_set<char>st;
+    int l=s.length();
+    for(int i=0;i<l;i++)
+    {
+        if(st.find(s[i])==st.end())
+        {
+            cout<<s[i]<<" ";
+            st.insert(s[i]);
+        }
+    }
     return 0;
 }
