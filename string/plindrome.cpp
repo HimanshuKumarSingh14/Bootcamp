b5e3d378fc269a89899f48af890f1feec751cefd
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-11 15:11:06 +0530

    palindrome

---
+++ string/plindrome.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s1;
+    cin>>s1;
+    string s2=s1;
+    reverse(s1.begin(),s1.end());
+    
+    if(s1==s2)
+    {
+        cout<<"Palindrome";
+    }
+    else{
+        cout<<"Not Palindrome";
+    }
+
+    return 0;
+}
