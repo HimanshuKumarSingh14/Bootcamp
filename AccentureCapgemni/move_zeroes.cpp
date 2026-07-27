72c861eabd44f227d7451b9c0ba3d6ce9df6355e
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-07-27 15:25:04 +0530

    move_zeroes

---
+++ AccentureCapgemni/move_zeroes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    int j;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+    for(int k=j+1;k<n;k++)
+    {
+        if(arr[k]!=0)
+        {
+            swap(arr[j],arr[k]);
+            j++;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
