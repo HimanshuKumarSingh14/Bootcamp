80c27738d6a5b4637b716c537c9c04c7d218c9f9
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-08-09 10:40:00 +0530

    question7

---
+++ practice_question/question7.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int max=arr[n-1];
+    cout<<max<<" ";
+    for(int i=n-2;i>=0;i--)
+    {
+        if(arr[i]>max)
+        {
+            cout<<arr[i]<<" ";
+            max=arr[i];
+        }
+    }
+    return 0;
+}
