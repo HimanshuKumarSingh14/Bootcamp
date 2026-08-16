452337e2ce4621b7217cdada68f1d8cd08f886aa
HimanshuKumarSingh14 <himanshukumar14122004@gmail.com>
2026-08-16 15:57:10 +0530

    question15

---
+++ practice_question/question16.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+   // cin>>s;
+   cout<<"Enter the string";
+    getline(cin,s);
+    for(int i=0;i<s.size();i++)
+    {
+        cin
+    }
+
+
+    return 0;
+}
